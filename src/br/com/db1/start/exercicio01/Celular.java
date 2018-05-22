43faa58854c5b8109727332ac3a335a81398abc3
katiasilva13 <katiax13@gmail.com>
2018-05-22 17:33:25 -0300

    exercicios 1 e 2

---
+++ src/br/com/db1/start/exercicio01/Celular.java
@@ -0,0 +1,22 @@
+package br.com.db1.start.exercicio01;
+
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+public class Celular {
+	
+	private String operadora;
+
+	private String marca;
+
+	private Boolean dualChip;
+
+	private Double tamanhoTela;
+
+	public Boolean temSinal() {
+		return true;
+	}
+
+	
+
+}
---
+++ src/br/com/db1/start/exercicio02/Roupa.java
@@ -0,0 +1,19 @@
+package br.com.db1.start.exercicio02;
+
+public class Roupa {
+
+	private Tamanho tamanho;
+
+	private String marca;
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	private String anoFabricacao;
+
+}
